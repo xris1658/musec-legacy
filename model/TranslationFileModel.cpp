3bb3b5cd19d2515640f8533064f44718ae47bcc6
xris1658 <chris1658@foxmail.com>
2022-08-12 15:30:55 +0800

    Add `TranslationFileModel`, fixes commit d18e3b0

---
+++ model/TranslationFileModel.cpp
@@ -0,0 +1,95 @@
+#include "TranslationFileModel.hpp"
+
+#include "i18n/I18N.hpp"
+
+#include <QCoreApplication>
+#include <QDir>
+
+namespace Musec::Model
+{
+TranslationFileModel::TranslationFileModel(QObject* parent): QAbstractListModel(parent)
+{
+    roleNames_.reserve(columnSize());
+    roleNames_[RoleNames::PathRole] = "path";
+    roleNames_[RoleNames::LanguageNameRole] = "language_name";
+    roleNames_[RoleNames::ReadableLanguageNameRole] = "readable_language_name";
+    QTranslator translator;
+    static QStringList nameFilter {"*.qm"};
+    static QDir appDir(QCoreApplication::applicationDirPath());
+    auto entryInfoList = appDir.entryInfoList(nameFilter, QDir::Filter::Files, QDir::SortFlag::Name);
+    auto& ret = list_;
+    ret.reserve(entryInfoList.size() + 1);
+    for(auto& entryInfo: entryInfoList)
+    {
+        const auto& path = entryInfo.absoluteFilePath();
+        bool loadPathResult = translator.load(path);
+        if(loadPathResult)
+        {
+            const auto& language = translator.language();
+            ret.emplace_back(
+                std::make_tuple(
+                    path,
+                    language,
+                    Musec::I18N::readableNameFromLanguageName(language)
+                )
+            );
+        }
+    }
+    ret.emplace_back("", "en_US", "English (US)");
+}
+
+TranslationFileModel::~TranslationFileModel()
+{
+
+}
+
+const TranslationFileModel::Item& TranslationFileModel::operator[](int index) const
+{
+    return list_[index];
+}
+
+int TranslationFileModel::itemCount() const
+{
+    return list_.size();
+}
+
+constexpr int TranslationFileModel::columnSize()
+{
+    return RoleNames::RoleNamesCount - Qt::UserRole;
+}
+
+int TranslationFileModel::rowCount(const QModelIndex&) const
+{
+    return itemCount();
+}
+
+int TranslationFileModel::columnCount(const QModelIndex&) const
+{
+    return columnSize();
+}
+
+QVariant TranslationFileModel::data(const QModelIndex& index, int role) const
+{
+    int row = index.row();
+    if(row < 0 || row >= rowCount(index))
+    {
+        return QVariant();
+    }
+    switch (role)
+    {
+    case RoleNames::PathRole:
+        return QVariant::fromValue(std::get<RoleNames::PathRole - Qt::UserRole>(list_[row]));
+    case RoleNames::LanguageNameRole:
+        return QVariant::fromValue(std::get<RoleNames::LanguageNameRole - Qt::UserRole>(list_[row]));
+    case RoleNames::ReadableLanguageNameRole:
+        return QVariant::fromValue(std::get<RoleNames::ReadableLanguageNameRole - Qt::UserRole>(list_[row]));
+    default:
+        return QVariant();
+    }
+}
+
+RoleNamesType TranslationFileModel::roleNames() const
+{
+    return roleNames_;
+}
+}
---
+++ model/TranslationFileModel.hpp
@@ -0,0 +1,48 @@
+#ifndef MUSEC_MODEL_TRANSLATIONFILEMODEL
+#define MUSEC_MODEL_TRANSLATIONFILEMODEL
+
+#include "model/ModelBase.hpp"
+
+#include <QAbstractListModel>
+
+#include <tuple>
+#include <vector>
+
+namespace Musec
+{
+namespace Model
+{
+class TranslationFileModel: public QAbstractListModel
+{
+    Q_OBJECT
+public:
+    using Item = std::tuple<QString, QString, QString>;
+    using List = std::vector<Item>;
+    enum RoleNames
+    {
+        PathRole = Qt::UserRole,
+        LanguageNameRole,
+        ReadableLanguageNameRole,
+        RoleNamesCount
+    };
+public:
+    explicit TranslationFileModel(QObject* parent = nullptr);
+    ~TranslationFileModel();
+public:
+    const Item& operator[](int index) const;
+    int itemCount() const;
+    static constexpr int columnSize();
+public:
+    virtual int rowCount(const QModelIndex&) const override;
+    virtual int columnCount(const QModelIndex&) const override;
+    virtual QVariant data(const QModelIndex& index, int role) const override;
+protected:
+    virtual RoleNamesType roleNames() const override;
+private:
+    List list_;
+    RoleNamesType roleNames_;
+};
+}
+}
+
+#endif //MUSEC_MODEL_TRANSLATIONFILEMODEL
