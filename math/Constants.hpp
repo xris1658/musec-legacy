1a58f2a935e372b78a6bcd7f7c872c7d6d523bb4
xris1658 <chris1658@foxmail.com>
2022-10-17 22:09:28 +0800

    Try using constants in the standard library if C++20 is used

--- math/Constants.hpp
+++ math/Constants.hpp
@@ -3,15 +3,27 @@
 
 #include <cmath>
 
+#if __cplusplus >= 202002L
+#include <numbers>
+#endif
+
 namespace Musec
 {
 namespace Math
 {
+#if __cplusplus >= 202002L
+template<typename T>
+constexpr T Pi = std::numbers::pi_v<T>;
+
+template<typename T>
+constexpr T Sqrt2 = std::numbers::sqrt2_v<T>;
+#else
 template<typename T>
 constexpr T Pi = std::acos(T(-1));
 
 template<typename T>
 constexpr T Sqrt2 = std::sqrt(T(2));
+#endif
 }
 }
 
